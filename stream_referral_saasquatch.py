b801cbdb64858c93632cc9deda44ff8487e4a2b0
Robert J. Moore <rmoore@rjmetrics.com>
2016-12-23 01:29:51 -0500

    ready for RS review, live and working on my test account

--- stream_referral_saasquatch.py
+++ stream_referral_saasquatch.py
@@ -40,19 +40,20 @@ def is_export_completed(tenant_alias, api_key, exportId):
         return True
     return False
 
-def get_id_of_completed_export(exportType, startingTimestamp, tenant_alias, api_key):
+def run_and_get_id_of_completed_export(exportType, tenant_alias, api_key):
     #this function is a multistep process of requesting an export, waiting for it to be generated
     #and then eventually returning the URL of the results once the generation is complete
+    startingTimestamp = bookmark.get(exportType, None) 
     createUrl = 'https://app.referralsaasquatch.com/api/v1/' + tenant_alias + '/export'
     data = {'type' : exportType,
             'format' : 'CSV',
             'name' : 'Stitch Streams ' + exportType + ' Request ' + str(datetime.datetime.now()),
+            'params' : {}
     }
     auth = ('', api_key)
     headers = { 'Content-Type' : 'application/json' }
     if startingTimestamp:
-        data.params = { 'createdOrUpdatedSince' : startingTimestamp }
-
+        data['params'] = { 'createdOrUpdatedSince' : startingTimestamp }
     r = requests.post(createUrl, auth=auth, json=data, headers=headers)
     resultJson = r.json()
 
@@ -68,43 +69,83 @@ def get_id_of_completed_export(exportType, startingTimestamp, tenant_alias, api_
     else:
         raise Exception('Request to create ' + exportType + ' export failed')
 
-def stream_file_contents(streamName, exportId, tenant_alias, api_key):
+def cleanse_value(name, value):
+    if name == "amount":
+        value = int(value)
+    elif "date" in name: #consistent pattern that all dates have "date" in their name
+        if value != '':
+            #Referral SaaSquatch returns timestamps in ms, so we convert to timestamp in seconds
+            valueSeconds = int(int(value)/1000)
+            dt = datetime.datetime.utcfromtimestamp(valueSeconds)
+            value = dt.isoformat() + '+00:00'
+        else:
+            value = None
+    return value
+
+def stream_export_contents_to_stitch(streamName, exportId, tenant_alias, api_key):
     downloadUrl = 'https://app.referralsaasquatch.com/api/v1/' + tenant_alias + '/export/' + exportId + '/download'
     auth = ('', api_key)
     headers = { 'Content-Type' : 'application/json' }
     r = requests.get(downloadUrl, stream=True, auth=auth, headers=headers)
-    
-    #get the mapping of column names from the header row
-    headerLine = [line.decode('utf-8') for line in r.iter_lines()]
-    headerJsonCsvReader = csv.reader([headerLine])
-    next(headerJsonCsvReader)
 
     #iterate through the rest of the lines, persisting to Stitch
-    logger.info('LINES:')
+    headerMapping = None
     for line in r.iter_lines():
         text = line.decode('utf-8')
         csvReader = csv.reader([text])
         for csvLine in csvReader:
-            logger.info(csvLine)
+            if headerMapping is None: #first line contains column names, save it
+                headerMapping = csvLine
+            else: #bundle up the row and write to Stitch
+                dataRows = [{}]
+                for index, value in enumerate(csvLine, start=0):
+                    dataRows[0][headerMapping[index]] = cleanse_value(headerMapping[index], value)
+                stitchstream.write_records(streamName, dataRows)
+
+def current_utc_timestamp_ms():
+    dts = time.time()
+    return int(dts)*1000
+
+def update_stream(streamType, tenant_alias, api_key):
+    global bookmark
+    if bookmark.get(streamType, None):
+        logger.info('Replicating ' + streamType + ' since %s', bookmark.get(streamType, None))
+    else:
+        logger.info('Replicating all ' + streamType)
+
+    processStartTimestamp = current_utc_timestamp_ms() #must bookmark in utc timestamp in ms
+    exportId = run_and_get_id_of_completed_export(streamType, tenant_alias, api_key)
+    stream_export_contents_to_stitch(streamType, exportId, tenant_alias, api_key)
+    bookmark[streamType] = processStartTimestamp #we don't get timestamps in the data so we bookmark by when the process ran
 
 user_schema = {'type': 'object',
                  'properties': {
                      'id': {
-                         'type': 'integer',
+                         'type': 'string',
                          'key': True
                      },
-                     'created_at': {
+                     'accountId': {
                          'type': 'string',
-                         'format': 'date-time'
-                     },
-                     'updated_at': {
-                         'type': 'string',
-                         'format': 'date-time'
                      },
                      'email': {
+                         'type': 'string',
+                     },
+                     'firstName': {
                          'type': 'string'
                      },
-                     'last_surveyed': {
+                     'lastName': {
+                         'type': 'string'
+                     },
+                     'imageUrl': {
+                         'type': 'string'
+                     },
+                     'firstSeenIP': {
+                         'type': 'string'
+                     },
+                     'lastSeenIP': {
+                         'type': 'string'
+                     },
+                     'dateCreated': {
                          "anyOf": [
                              {
                                  "type": "null",
@@ -115,54 +156,133 @@ user_schema = {'type': 'object',
                              }
                          ]
                      },
-                     'external_created_at': {
-                         'type': ['integer','null']
+                     'emailHash': {
+                         'type': 'string'
                      },
-                     'page_views_count': {
-                         'type': 'integer'
+                     'referralSource': {
+                         'type': 'string'
+                     },
+                     'locale': {
+                         'type': 'string'
+                     },
+                     'shareLink': {
+                         'type': 'string'
+                     },
+                     'facebookShareLink': {
+                         'type': 'string'
+                     },
+                     'twitterShareLink': {
+                         'type': 'string'
+                     },
+                     'emailShareLink': {
+                         'type': 'string'
+                     },
+                     'linkedinShareLink': {
+                         'type': 'string'
                      }
                  },
                  'required': ['id']
              }
 
-def get_all_new_users(tenant_alias, api_key):
-    global bookmark
-    
-    startingTimestamp = bookmark.get('users', None) 
-    exportId = get_id_of_completed_export('USER', startingTimestamp, tenant_alias, api_key)
-    stream_file_contents('users', exportId, tenant_alias, api_key)
+reward_balance_schema = {'type': 'object',
+                 'properties': {
+                     'userId': {
+                         'type': 'string',
+                         'key': True
+                     },
+                     'accountId': {
+                         'type': 'string',
+                     },
+                     'type': {
+                         'type': 'string',
+                     },
+                     'amount': {
+                         'type': 'integer'
+                     },
+                     'unit': {
+                         'type': 'string'
+                     }
+                 },
+                 'required': ['userId']
+             }
 
-    return exportId
-
-    last_user_unixtime = None
-    url = 'https://app.referralsaasquatch.com/api/v1/' + tenant_alias + '/users'
-    r = requests.get(url, auth=('', api_key))
-    logger.info(r.json())
-    requestUrl = '/api/v1/{tenant_alias}/export'
-    for response in authed_get_all_pages(requestUrl, 'users'):
-        users = response.json()
-        if len(users) > 0:
-            last_created_at_datetime = wootricdate_to_datetime(users[-1]['created_at'])
-            last_user_unixtime = int(last_created_at_datetime.timestamp())
-
-        for index, item in enumerate(users):
-            users[index]['created_at'] = wootricdate_to_datetime(users[index]['created_at']).isoformat()
-            users[index]['updated_at'] = wootricdate_to_datetime(users[index]['updated_at']).isoformat()
-            if users[index]['last_surveyed']:
-                users[index]['last_surveyed'] = wootricdate_to_datetime(users[index]['last_surveyed']).isoformat()
-
-        stitchstream.write_records('users', users)
-
-        #there is a limitation of wootric's API that only allows you to get 50 records at a time and has
-        #no pagination trigger other than created_at date; as such if >50 records have the same created_at
-        #date you hit an infinite loop of requests; this breaks you out of that loop if it happens
-        if bookmark.get('users', None) == str(last_user_unixtime) and len(users) > 1:
-            logger.error('Breaking retrieval loop for enduers at unixtime ' + str(last_user_unixtime) + ', will cause missing data')
-            last_user_unixtime = last_user_unixtime + 1
-
-        if last_user_unixtime: #can be None if no new users
-            bookmark['users'] = str(last_user_unixtime)
-
+referral_schema = {'type': 'object',
+                 'properties': {
+                     'id': {
+                         'type': 'string',
+                         'key': True
+                     },
+                     'referredUser': {
+                         'type': 'string',
+                     },
+                     'referredAccount': {
+                         'type': 'string',
+                     },
+                     'referrerUser': {
+                         'type': 'string'
+                     },
+                     'referrerAccount': {
+                         'type': 'string'
+                     },
+                     'referredReward': {
+                         'type': 'string'
+                     },
+                     'referrerReward': {
+                         'type': 'string'
+                     },
+                     'dateReferralStarted': {
+                         "anyOf": [
+                             {
+                                 "type": "null",
+                             }, 
+                             {
+                                 "type": "string",
+                                 'format': 'date-time'
+                             }
+                         ]
+                     },
+                     'dateReferralPaid': {
+                         "anyOf": [
+                             {
+                                 "type": "null",
+                             }, 
+                             {
+                                 "type": "string",
+                                 'format': 'date-time'
+                             }
+                         ]
+                     },
+                     'dateReferralEnded': {
+                         "anyOf": [
+                             {
+                                 "type": "null",
+                             }, 
+                             {
+                                 "type": "string",
+                                 'format': 'date-time'
+                             }
+                         ]
+                     },
+                     'dateModerated': {
+                         "anyOf": [
+                             {
+                                 "type": "null",
+                             }, 
+                             {
+                                 "type": "string",
+                                 'format': 'date-time'
+                             }
+                         ]
+                     },
+                     'referredModerationStatus': {
+                         'type': 'string'
+                     },
+                     'referrerModerationStatus': {
+                         'type': 'string'
+                     },
+                 },
+                 'required': ['id']
+             }
 
 if __name__ == '__main__':
     configure_logging()
@@ -179,11 +299,13 @@ if __name__ == '__main__':
             for line in file:
                 bookmark = json.loads(line.strip())
 
-    if bookmark.get('users', None):
-        logger.info('Replicating users since %s', bookmark.get('users', None))
-    else:
-        logger.info('Replicating all users')
-    #stitchstream.write_schema('users', user_schema)
-    get_all_new_users(tenant_alias, api_key)
+    stitchstream.write_schema('USER', user_schema)
+    update_stream('USER', tenant_alias, api_key)
+
+    stitchstream.write_schema('REWARD_BALANCE', reward_balance_schema)
+    update_stream('REWARD_BALANCE', tenant_alias, api_key)
+
+    stitchstream.write_schema('REFERRAL', referral_schema)
+    update_stream('REFERRAL', tenant_alias, api_key)
 
     stitchstream.write_bookmark(bookmark)
